8c1000c168f2ea0f218e7dccbf41eee81fc31133
Tomas Jansson <tomas.jansson@gmail.com>
2015-10-07 08:38:28 +0200

    initial commit

---
+++ StuffInCSharp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuffInCSharp
+{
+    public abstract class User
+    {
+        protected User(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public class DeletedUser : User
+    {
+        public DateTime DeletionDate { get; private set; }
+
+        public DeletedUser(ActiveUser user, DateTime deletionDate) : base(user.Name)
+        {
+            DeletionDate = deletionDate;
+        }
+
+        public ActiveUser UndeleteUser()
+        {
+            return new ActiveUser(this.Name, new List<Subscription>());
+        }
+    }
+
+    public class ActiveUser : User
+    {
+        public List<Subscription> Subscriptions { get; private set; }
+
+        public ActiveUser(string name, List<Subscription> subscriptions) : base(name)
+        {
+            Subscriptions = subscriptions;
+        }
+
+
+        public void AddSubscription(Subscription sub)
+        {
+            Subscriptions.Add(sub);
+        }
+
+        public DeletedUser DeleteUser(DateTime dateTime)
+        {
+            return new DeletedUser(this, dateTime);
+        }
+    }
+
+    public class Subscription
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StuffInFSharp/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module User = 
+    open System
+
+    type Subscription = Subscription of string
+    type ActiveUser = {Name: string; Subscriptions: Subscription list}
+    type DeletedUser = {Name: string; DeletionDate: DateTime}
+
+    type User = 
+    | ActiveUser of ActiveUser
+    | DeletedUser of DeletedUser
+
+    let delete activeUser = 
+        let {Name = name; Subscriptions = _} = activeUser
+        {Name = name; DeletionDate = DateTime.UtcNow}
+
+    let undelete deletedUser = 
+        let {Name = name; DeletionDate = _} = deletedUser
+        {Name = name; Subscriptions = []}
+
+    let addSubscription activeUser newSub = 
+        {activeUser with Subscriptions = newSub::activeUser.Subscriptions}
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
